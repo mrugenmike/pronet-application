fcc0d3c5d1221a3deb0cd8566441ba7870bffd93
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-03-13 19:24:33 -0700

    Checking in basic login logout

--- app/app.js
+++ app/app.js
@@ -22,8 +22,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Client Session
+const session = require("client-sessions");
+
+//https://stormpath.com/blog/everything-you-ever-wanted-to-know-about-node-dot-js-sessions/
+app.use(session({
+    cookieName: 'session',
+    secret: 'random_string_goes_here',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000
+}));
 app.use('/', routes);
 app.use('/users', users);
+
+var user = {uname:"mrugen",password:"password"};
+app.use(function(req, res, next) {
+    if (req.session && req.session.user) {
+        {
+            if (user) {
+                req.user = user;
+                delete req.user.password; // delete the password from the session
+                req.session.user = user;  //refresh the session value
+                res.locals.user = user;
+            }
+            // finishing processing the middleware and run the route
+            next();
+        };
+    } else {
+        next();
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +83,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+function requireLogin (req, res, next) {
+    console.log("requirelogin");
+    if (!req.session.user) {
+        console.log("req.user not found");
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+router.get('/',function(req, res) {
+  res.redirect("/dashboard");
 });
 
+router.get("/login",function(req,res){
+    res.render("login.ejs",{error:"Please Login"});
+});
+
+
+var user = {uname:"mrugen",password:"password"};
+
+router.post('/auth',function(req,res){
+    console.log("auth: "+req.body.uname);
+        if (req.body.uname===user.uname) {
+            if (req.body.password === user.password) {
+                // sets a cookie with the user's info
+                req.session.user = user;
+                res.redirect('/dashboard');
+            } else {
+                res.render('login.ejs', { error: 'Invalid email or password.' });
+            }
+
+        } else {
+            res.render('login.ejs', { error: 'Invalid email or password.' });
+        }
+});
+
+router.post('/logout', function (req, res) {
+    req.session.reset();
+    res.redirect('/');
+});
+
+
+router.get("/dashboard",requireLogin,function(req,res){
+    console.log("dashboard");
+    res.render("dashboard",{"name":"dummy"});
+})
 module.exports = router;
